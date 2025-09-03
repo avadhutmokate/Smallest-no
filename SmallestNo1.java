0c115d7c85a1b1d451992ecb35541e2ca8a063b5
Avadhut Mokate <mokateavadhut@gmail.com>
2025-09-03 08:29:34 +0530

    Add files via upload

---
+++ SmallestNo1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class SmallestNo1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a num1 : ");
+        int a = sc.nextInt();
+        System.out.println("Enter a num2 : ");
+        int b = sc.nextInt();
+
+        if ( a < b)
+        { System.out.println("1st no is smallest"); }
+        if
+        ( !(a<b ))
+        { System.out.println("2nd no  is smallest"); }
+        
+    }
+}
